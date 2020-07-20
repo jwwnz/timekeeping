245376247cdf4464cc1c06c2e24c3336e20d8faf
jwwnz <jwon117@aucklanduni.ac.nz>
2020-07-20 21:41:34 +1200

    extract out test data and import instead

--- src/App.js
+++ src/App.js
@@ -2,31 +2,7 @@ import React, { useState } from "react";
 import "./App.css";
 import dayjs from "dayjs";
 import "font-awesome/css/font-awesome.min.css";
-
-const something = [
-	{
-		id: "abc123",
-		caseId: "ABC-123",
-		startTime: new Date(),
-		unit: 1.0,
-		type: "Pleadings",
-	},
-	{
-		id: "bcd321",
-		caseId: "ABC-123",
-		startTime: new Date(),
-		unit: 0.5,
-		type: "Pleadings",
-	},
-];
-
-const newAdd = {
-	id: "zbc",
-	caseId: "ABC-123",
-	startTime: new Date(),
-	unit: 1,
-	type: "Pleadings",
-};
+import { something, newAdd } from "./testData.js";
 
 const Navbar = () => (
 	<header className="App-header">
---
+++ src/testData.js
@@ -0,0 +1,26 @@
+const something = [
+	{
+		id: "abc123",
+		caseId: "ABC-123",
+		startTime: new Date(),
+		unit: 1.0,
+		type: "Pleadings",
+	},
+	{
+		id: "bcd321",
+		caseId: "ABC-123",
+		startTime: new Date(),
+		unit: 0.5,
+		type: "Pleadings",
+	},
+];
+
+const newAdd = {
+	id: "zbc",
+	caseId: "ABC-123",
+	startTime: new Date(),
+	unit: 1,
+	type: "Pleadings",
+};
+
+export { something, newAdd };
