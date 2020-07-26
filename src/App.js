fc001b239abae790ea7c66f61c7f0938fe0315d7
jwwnz <jameswon.nz@gmail.com>
2020-07-26 22:17:22 +1200

    Add ability to add directly from the modal, functional without styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./App.css";
 import "font-awesome/css/font-awesome.min.css";
-// import { v4 as uuidv4 } from "uuid";
+import { v4 as uuidv4 } from "uuid";
 import { something } from "./testData.js";
 
 const Navbar = () => (
@@ -14,13 +14,6 @@ function App() {
 	const [entries, setEntries] = useState(something);
 	const [modalOpen, setModalOpen] = useState(false);
 
-	const addEntry = () => {
-		setModalOpen(true);
-		console.log("Added a new entry")
-		// setNewEntry({ ...newEntry, id: uuidv4() });
-		// setEntries([...entries, newEntry]);
-	};
-
 	const toggleEditModal = () => {
 		setModalOpen(!modalOpen);
 	}
@@ -31,6 +24,133 @@ function App() {
 		});
 		setEntries(arrayWithoutDeletedEntry);
 	};
+
+	const Modal = () => {
+		const [newEntry, setNewEntry] = useState({
+			id: null,
+			caseId: null,
+			startTime: null,
+			endTime: null,
+			unit: 0,
+			type: null,
+		});
+
+		const addEntry = () => {
+			setNewEntry({ ...newEntry, id: uuidv4() });
+			setEntries([...entries, newEntry]);
+			toggleEditModal();
+		};
+
+		const updateNewStartTime = (e) => {
+			setNewEntry({
+				caseId: newEntry.caseId,
+				startTime: e.target.value,
+				endTime: newEntry.endTime,
+				unit: newEntry.unit,
+				type: newEntry.type,
+			});
+		};
+
+		const updateNewEndTime = (e) => {
+			setNewEntry({
+				caseId: newEntry.caseId,
+				startTime: newEntry.startTime,
+				endTime: e.target.value,
+				unit: newEntry.unit,
+				type: newEntry.type,
+			});
+		};
+
+		const updateNewUnit = (e) => {
+			setNewEntry({
+				caseId: newEntry.caseId,
+				startTime: newEntry.startTime,
+				endTime: newEntry.endTime,
+				unit: e.target.value,
+				type: newEntry.type,
+			});
+		};
+
+		const updateNewCaseId = (e) => {
+			setNewEntry({
+				caseId: e.target.value,
+				startTime: newEntry.startTime,
+				endTime: newEntry.endTime,
+				unit: newEntry.unit,
+				type: newEntry.type,
+			});
+		};
+
+		const updateNewType = (e) => {
+			setNewEntry({
+				caseId: newEntry.caseId,
+				startTime: newEntry.startTime,
+				endTime: newEntry.endTime,
+				unit: newEntry.unit,
+				type: e.target.value,
+			});
+		};
+
+
+		return (<div id="myModal" class="modal">
+			<div class="modal-content">
+				<span class="close" onClick={toggleEditModal}>&times;</span>
+				<h2>Add a new entry</h2>
+
+				<div className="Time-entry Time-entry-add" key={"add"}>
+					<div>
+						<input
+							className="input input-time"
+							type="time"
+							id="time-start"
+							name="time-start"
+							onChange={updateNewStartTime}
+						/>
+						<input
+							className="input input-time"
+							type="time"
+							id="time-end"
+							name="time-end"
+							onChange={updateNewEndTime}
+						/>
+					</div>
+					<div>
+						<input
+							className="input input-unit"
+							type="number"
+							id="unit"
+							name="unit"
+							onChange={updateNewUnit}
+						/>
+					&nbsp;unit
+				</div>
+					<div>
+						<input
+							className="input input-case-id"
+							type="text"
+							id="case-id"
+							name="case-id"
+							placeholder="case id"
+							onChange={updateNewCaseId}
+						/>
+					</div>
+					<div>
+						<input
+							className="input input-type"
+							type="text"
+							id="type"
+							name="type"
+							placeholder="Type"
+							onChange={updateNewType}
+						/>
+					</div>
+					<button onClick={addEntry}>
+						<i className="fa fa-plus"></i>
+					</button>
+				</div>
+			</div>
+		</div>);
+	}
 
 	return (
 		<div className="App">
@@ -60,13 +180,7 @@ function App() {
 			{/* This is modal content created by the button */}
 			{
 				modalOpen && (
-					<div id="myModal" class="modal">
-						<div class="modal-content">
-							<span class="close" onClick={toggleEditModal}>&times;</span>
-							<h2>Add a new entry</h2>
-
-						</div>
-					</div>
+					<Modal />
 				)
 			}
 
